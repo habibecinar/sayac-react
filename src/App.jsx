fb4cfa9980df938c73735d1351d911f9786f9d41
Habibe Çınar <146085701+habibecinar@users.noreply.github.com>
2025-04-29 10:29:46 +0300

    localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-    const [sayi, setSayi] = useState(0);
-
-
-    return(
+  const [sayi, setSayi] = useState(() => {
+    const kayitli = localStorage.getItem("sayi");
+    return kayitli ? parseInt(kayitli) : 0;
+  });
+  useEffect(() => {
+    localStorage.setItem("sayi", sayi);
+  }, [sayi]);
+  return (
     <>
-        <h1> {sayi} </h1>
-    <button onClick={() => setSayi(sayi + 1)}>Artır</button>
-        <button onClick={() => setSayi(sayi - 1)} disabled={sayi===0} >Azalt</button>
-            <button onClick={() => setSayi(0)}>Sıfırla</button>
-        </>
-    )
-}  
+      <h1 className="counter">{sayi} </h1>
+      <button onClick={() => setSayi(sayi + 1)}>Artır</button>
+      <button onClick={() => setSayi(sayi - 1)} disabled={sayi === 0}>
+        Azalt
+      </button>
+      <button onClick={() => setSayi(0)}>Sıfırla</button>
+    </>
+  );
+}
 export default App;
